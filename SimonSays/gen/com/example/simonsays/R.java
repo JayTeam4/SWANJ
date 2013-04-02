e940036d5f66ff72292343b628b995a010aee6fd
Jay_Team4 <jbl1102@jagmail.southalabama.com>
2013-04-02 11:16:59 -0500

    added spinner int array

--- SimonSays/gen/com/example/simonsays/R.java
+++ SimonSays/gen/com/example/simonsays/R.java
@@ -8,6 +8,9 @@
 package com.example.simonsays;
 
 public final class R {
+    public static final class array {
+        public static final int numOfObjects=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,64 +27,64 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button02=0x7f080018;
-        public static final int RelativeLayout1=0x7f080000;
-        public static final int action_settings=0x7f080028;
-        public static final int button1=0x7f080002;
-        public static final int button2=0x7f08000e;
-        public static final int checkBox1=0x7f080006;
-        public static final int checkBox2=0x7f080005;
-        public static final int checkBox3=0x7f080004;
-        public static final int checkBox4=0x7f080007;
-        public static final int checkBox5=0x7f080009;
-        public static final int checkBox6=0x7f08000a;
-        public static final int checkBox7=0x7f08000b;
-        public static final int checkBox8=0x7f08000c;
-        public static final int checkBox9=0x7f08000d;
-        public static final int editText1=0x7f080017;
-        public static final int radio0=0x7f080014;
-        public static final int radio1=0x7f080015;
-        public static final int radioGroup1=0x7f080013;
-        public static final int space1=0x7f08001c;
-        public static final int space2=0x7f08001d;
-        public static final int space3=0x7f08001b;
-        public static final int spinner1=0x7f080016;
-        public static final int tableRow1=0x7f08001a;
-        public static final int tableRow2=0x7f08001e;
-        public static final int tableRow3=0x7f080020;
-        public static final int tableRow4=0x7f080022;
-        public static final int tableRow5=0x7f080024;
-        public static final int tableRow6=0x7f080026;
-        public static final int textView1=0x7f080001;
-        public static final int textView10=0x7f080023;
-        public static final int textView11=0x7f080025;
-        public static final int textView12=0x7f080027;
-        public static final int textView2=0x7f080003;
-        public static final int textView3=0x7f080011;
-        public static final int textView4=0x7f080008;
-        public static final int textView5=0x7f080010;
-        public static final int textView6=0x7f08000f;
-        public static final int textView7=0x7f080012;
-        public static final int textView8=0x7f08001f;
-        public static final int textView9=0x7f080021;
-        public static final int view1=0x7f080019;
+        public static final int Button02=0x7f090018;
+        public static final int RelativeLayout1=0x7f090000;
+        public static final int action_settings=0x7f090028;
+        public static final int button1=0x7f090002;
+        public static final int button2=0x7f09000e;
+        public static final int checkBox1=0x7f090006;
+        public static final int checkBox2=0x7f090005;
+        public static final int checkBox3=0x7f090004;
+        public static final int checkBox4=0x7f090007;
+        public static final int checkBox5=0x7f090009;
+        public static final int checkBox6=0x7f09000a;
+        public static final int checkBox7=0x7f09000b;
+        public static final int checkBox8=0x7f09000c;
+        public static final int checkBox9=0x7f09000d;
+        public static final int editText1=0x7f090017;
+        public static final int radio0=0x7f090014;
+        public static final int radio1=0x7f090015;
+        public static final int radioGroup1=0x7f090013;
+        public static final int space1=0x7f09001c;
+        public static final int space2=0x7f09001d;
+        public static final int space3=0x7f09001b;
+        public static final int spinner1=0x7f090016;
+        public static final int tableRow1=0x7f09001a;
+        public static final int tableRow2=0x7f09001e;
+        public static final int tableRow3=0x7f090020;
+        public static final int tableRow4=0x7f090022;
+        public static final int tableRow5=0x7f090024;
+        public static final int tableRow6=0x7f090026;
+        public static final int textView1=0x7f090001;
+        public static final int textView10=0x7f090023;
+        public static final int textView11=0x7f090025;
+        public static final int textView12=0x7f090027;
+        public static final int textView2=0x7f090003;
+        public static final int textView3=0x7f090011;
+        public static final int textView4=0x7f090008;
+        public static final int textView5=0x7f090010;
+        public static final int textView6=0x7f09000f;
+        public static final int textView7=0x7f090012;
+        public static final int textView8=0x7f09001f;
+        public static final int textView9=0x7f090021;
+        public static final int view1=0x7f090019;
     }
     public static final class layout {
         public static final int activity_config_screen=0x7f030000;
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int dontHaveUsername=0x7f050005;
-        public static final int enterUsername=0x7f050003;
-        public static final int hello_world=0x7f050002;
-        public static final int highScore=0x7f050007;
-        public static final int login=0x7f050004;
-        public static final int newUser=0x7f050006;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int dontHaveUsername=0x7f060005;
+        public static final int enterUsername=0x7f060003;
+        public static final int hello_world=0x7f060002;
+        public static final int highScore=0x7f060007;
+        public static final int login=0x7f060004;
+        public static final int newUser=0x7f060006;
     }
     public static final class style {
         /** 
@@ -105,10 +108,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
