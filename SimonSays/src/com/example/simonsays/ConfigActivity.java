96f77a4a2d8ee7bae6b8e6d1d3aa42ef0dfb646e
Jay_Team4 <jbl1102@jagmail.southalabama.com>
2013-04-09 13:34:20 -0500

    Added intents for everything on config screen so it can be passed to Play

--- SimonSays/src/com/example/simonsays/ConfigActivity.java
+++ SimonSays/src/com/example/simonsays/ConfigActivity.java
@@ -8,7 +8,11 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 
@@ -69,6 +73,74 @@ public class ConfigActivity extends Activity
             public void onClick(View arg0) 
             {
                 Intent intent = new Intent(context, PlayActivity.class);
+                //get number of objects
+                Spinner mySpinner = (Spinner)findViewById(R.id.spinner1);
+                String spinText = mySpinner.getSelectedItem().toString();
+              
+                intent.putExtra("numOfObjects", spinText);
+                //get shape
+                CheckBox square = (CheckBox)findViewById(R.id.checkBox1);
+                CheckBox triangle = (CheckBox)findViewById(R.id.checkBox2);
+                CheckBox circle = (CheckBox)findViewById(R.id.checkBox3);
+                
+                if (square.isChecked())
+                {
+                    intent.putExtra("square", (String)square.getText());
+                }
+                if (triangle.isChecked())
+                {
+                    intent.putExtra("triangle", (String)triangle.getText());
+                }
+                if (circle.isChecked())
+                {
+                    intent.putExtra("circle", (String)circle.getText());
+                }
+                //get color
+                CheckBox red = (CheckBox)findViewById(R.id.checkBox4);
+                CheckBox blue = (CheckBox)findViewById(R.id.checkBox5);
+                CheckBox green = (CheckBox)findViewById(R.id.checkBox6);
+                CheckBox purple = (CheckBox)findViewById(R.id.checkBox7);
+                CheckBox orange = (CheckBox)findViewById(R.id.checkBox8);
+                CheckBox yellow = (CheckBox)findViewById(R.id.checkBox9);
+                
+                if (red.isChecked())
+                {
+                    intent.putExtra("red", (String)red.getText());
+                }
+                if (blue.isChecked())
+                {
+                    intent.putExtra("blue", (String)blue.getText());
+                }
+                if (green.isChecked())
+                {
+                    intent.putExtra("green", (String)green.getText());
+                }
+                if (purple.isChecked())
+                {
+                    intent.putExtra("purple", (String)purple.getText());
+                }
+                if (orange.isChecked())
+                {
+                    intent.putExtra("orange", (String)orange.getText());
+                }
+                if (yellow.isChecked())
+                {
+                    intent.putExtra("yellow", (String)yellow.getText());
+                }
+                //get layout
+                RadioGroup myRadioGroup = (RadioGroup)findViewById(R.id.radioGroup1);
+                int selected = myRadioGroup.getCheckedRadioButtonId();
+                RadioButton buttonSelected = (RadioButton)findViewById(selected);
+                String layoutText = (String) buttonSelected.getText();
+                               
+                intent.putExtra("layout", layoutText);
+                
+                //get object size
+                Spinner mySpinner2 = (Spinner)findViewById(R.id.spinner2);
+                String spinText2 = mySpinner2.getSelectedItem().toString();
+                
+                intent.putExtra("objectSize",spinText2);
+                //start
                 startActivity(intent);
             }//end of onCLick
         });//end of setOnClickListener
